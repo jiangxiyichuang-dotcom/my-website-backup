971779e13475ea18484d09b6b3cc70dfda5a8145
jiangxiyichuang-dotcom <jiangxiyichuang@gmail.com>
2025-11-25 10:32:10 +0800

    Add index.html for homepage

--- script/GRPkFF.js
+++
@@ -1,1 +0,0 @@
-(function(window,$,undefined){var phoenixSite=window.phoenixSite||(window.phoenixSite={});var name="component_album";var albumList=phoenixSite.albumList||(phoenixSite.albumList={});$.extend(albumList,{init:function(){}})})(window,jQuery);
